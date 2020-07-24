7c9304bc1b6f97094a1cf46c6ee387d71218e09f
Wouter van den Heuvel <exo_00@hotmail.com>
2020-07-24 14:06:34 +0200

    feat: bridge

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Map from "./components/Map";
 import Modal from 'components/Modal';
+import PlayerBridge from 'components/playerBridge';
+import { GameData } from 'components/playerBridge/GameData';
+import { Content } from 'data/Content';
 
 
 function App() {
-  const [currentMap, setCurrentMap] = useState("maps/testmap3.json");
+  const [currentMap, setCurrentMap] = useState<string>();
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleClose = () => {
     setModalOpen(false)
   }
   const handleOpen = () => {
-    setModalOpen(false)
+    setModalOpen(true)
   }
+
+  const handleGameDataReceived = (data: GameData<Content>) => {
+    //setContent(data.content);
+    console.log(data)
+    setCurrentMap(data.content.mapJson);
+  }
+
+  useEffect(() => {
+    //setTimeout(handleOpen, 5000);
+  }, []);
+   
   return (
-    <div className="App" onMouseUp={handleOpen}>
+    <div className="App" onDoubleClick={handleOpen}>
+      <PlayerBridge gameDataReceived={handleGameDataReceived}/>
       <select value={currentMap} onChange={e => setCurrentMap(e.currentTarget.value)}>
         <option>maps/testmap1.json</option>
         <option>maps/testmap2.json</option>
@@ -23,141 +38,8 @@ function App() {
         <option>maps/testmap4.json</option>
       </select>
       {modalOpen && <Modal onClose={handleClose} />  }
-      <Map jsonPath={currentMap} />
-        {/* <Stage width={mapWidth} height={mapHeight} options={{backgroundColor: 0x0}} className="background">
-            {textures && (
-              <>
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([0, 0])}
-                /> 
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([1, 0])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([2, 0])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([3, 0])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-dadada.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([4, 0])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([5, 0])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-dadada.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([0, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-wood.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([1, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-blackwhite.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([2, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([3, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([4, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([5, 1])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([0, 2])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([1, 2])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([2, 2])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([3, 2])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([4, 2])}
-                />
-                <Sprite
-                  texture={textures["tile-structure-floor-grass.png"]}
-                  anchor={[.5, 1]}
-                  width={TILE_WIDTH}
-                  height={TILE_HEIGHT}
-                  position={tileLocationToPosition([5, 2])}
-                />
-              </> 
-            )}
-
-        </Stage> */}
-        </div>
+      {currentMap && <Map jsonPath={currentMap} /> }
+    </div>
   );
 }
 
