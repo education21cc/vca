39f8e74965bee0136297e6055ea0b09a4a8a7dc6
Wouter van den Heuvel <exo_00@hotmail.com>
2020-08-26 11:33:57 +0200

    Updates

--- precache-manifest.ad73f00f50f00562486ae5c85e7e7656.js
+++ precache-manifest.ad73f00f50f00562486ae5c85e7e7656.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e8bf0b764f8126b5ca8e4f17052fb46",
+    "revision": "136e501007d21435c50103bbc9f3758e",
     "url": "/vca/index.html"
   },
   {
-    "revision": "7b4bdd6f1f03d293b7cd",
+    "revision": "1968d581d52541dcc843",
     "url": "/vca/static/css/main.a7133ced.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vca/static/js/2.82b28aa3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b4bdd6f1f03d293b7cd",
-    "url": "/vca/static/js/main.cf2176ba.chunk.js"
+    "revision": "1968d581d52541dcc843",
+    "url": "/vca/static/js/main.42ab839b.chunk.js"
   },
   {
     "revision": "b001fa3831595630c1a5",
