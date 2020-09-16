85e6135da1047bb3d2328e1616d7c45b4ab1018b
Wouter van den Heuvel <exo_00@hotmail.com>
2020-09-16 23:20:37 +0200

    chore: cleanup

--- src/data/Content.ts
+++ src/data/Content.ts
@@ -26,7 +26,6 @@ export interface ContentConfig {
 export interface Situation {
   url: string;
   title: string;
-  // todo: inner game config!?
 }
 
 export interface Scenario {
@@ -73,58 +72,3 @@ export interface SequenceItem {
   scene?: SceneElement[]; // optional override
 }
 
-// Something
-
-// export interface ConflictContent {
-//   description: string;
-//   sequence: SequenceItem[];
-//   scene: SceneElement[];
-//   situationSpeech: string;
-//   options: string[];
-//   reactions: ConflictReaction[];
-// }
-
-// export interface SceneElement {
-//   image?: string;
-//   type?: SceneElementType;
-//   position?: [number, number];
-//   scale?: number;
-//   flipped: boolean;
-//   pose: AvatarPose;
-// }
-
-// export enum SceneElementType {
-//   sprite = "sprite",
-//   avatar = "avatar",
-// }
-
-// export enum AvatarPose {
-//   angle = "angle",
-//   front = "front",
-//   side = "side",
-//   angry = "angry"
-// }
-
-// export interface ConflictReaction { 
-//   correct: boolean, 
-//   text: string, 
-//   scene: SceneElement[]; 
-//   confirmText: string,
-//   yesText: string;
-//   noText: string;
-//   confirmImage?: string 
-// }
-
-// export enum SequenceItemType {
-//   caption = 'caption',
-//   speech = 'speech',
-//   image = 'image',  // image, no speech
-// }
-
-// export interface SequenceItem {
-//   type: SequenceItemType,
-//   text: string,
-//   balloonArrowPos?: number;
-  
-//   scene?: SceneElement[]; // optional override
-// }
